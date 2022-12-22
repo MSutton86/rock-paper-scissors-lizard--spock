90118347b6936a348628916d3da0f8e0ec762603
Mark S <stcoronawraps@gmail.com>
2022-12-22 14:25:07 -0700

    merge conflict auto resolved not auto resolved. manually resolved

--- player.py
+++ player.py
@@ -1,26 +1,29 @@
 class Player:
     def __init__(self) -> None:
-        self.name = self.gestures = ["rock", "scissors", "paper", "Lizard", "Spock"]
+        self.name = self
+        self.gestures = ["rock", "scissors", "paper", "Lizard", "Spock"]
         self.score = 0
-
-
-
-def game_choice(self, player):
-    self.single_player = one_v_ai
-    self.multi_player = one_v_one
 
     def choose_gesture(self):
         pass
-    def score_points(self):
-        pass
 
-"rock" > "scissors"
-"scissors" > "paper"
-"paper" > "rock"
-"rock" > "lizard"
-"lizard" > "spock"
-"spock" > "scissors"
-"scissors" > "lizard"
-"lizard" > "paper"
-"paper" > "spock"
-"spock" > "rock"
+    def score_points(self):
+        self.score += 1
+
+
+# def game_choice(self, player):
+#     self.single_player = one_v_ai
+#     self.multi_player = one_v_one
+
+# "rock" > "scissors"
+# "scissors" > "paper"
+# "paper" > "rock"
+# "rock" > "lizard"
+# "lizard" > "spock"
+# "spock" > "scissors"
+# "scissors" > "lizard"
+# "lizard" > "paper"
+# "paper" > "spock"
+# "spock" > "rock"
+
+d
